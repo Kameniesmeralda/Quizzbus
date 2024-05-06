50f9110cce243992daa2a3369f7dc8ef56f58fec
FEUNE WAFEU Aaron Bradley <aaronfeune@gmail.com>
2024-05-06 17:17:51 +0200

    Traduction en Francais Aaron

--- src/test/git/Test_FWAB.java
+++ src/test/git/Test_FWAB.java
@@ -12,13 +12,13 @@ public class Test_FWAB extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		
-		System.out.println( "Begin"  );
+		System.out.println( "Début"  );
 
 		var root = FXMLLoader.<Parent>load( getClass().getResource("View_FWAB.fxml") );
 		stage.setScene( new Scene(root) );
 		stage.show();
 		
-		System.out.println( "End"  );
+		System.out.println( "Fin"  );
 	}
 	
 	public static void main(String[] args) {
