7ce69f1e3db43077a2473ae51cd6cb91d4349d18
Esmeralda Kameni <kamenia@3il.fr>
2024-05-15 00:15:37 +0200

    managerGui

--- src/quizzbus/view/ManagerGui.java
+++ src/quizzbus/view/ManagerGui.java
@@ -10,7 +10,7 @@ import jfox.javafx.view.ManagerGuiAbstract;
 import jfox.javafx.view.View;
 
 public class ManagerGui extends ManagerGuiAbstract {
-	
+	////////////
 	//-------
 	// Actions
 	//-------
